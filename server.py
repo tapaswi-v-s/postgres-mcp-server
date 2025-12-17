48700b49fee1ba70b00cbebf39aa0648d159386c
Tapaswi <tapaswi_satyapanthi@intuit.com>
2025-12-17 00:21:16 -0500

    removed port

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ class DatabaseConnection:
     def _initialize_connection(self):
         """Initialize database connection"""
         db_host = os.getenv("DB_HOST")
-        db_port = os.getenv("DB_PORT", "5432")
+        # db_port = os.getenv("DB_PORT", "5432")
         db_name = os.getenv("DB_NAME")
         db_user = os.getenv("DB_USER")
         db_password = os.getenv("DB_PASSWORD")
